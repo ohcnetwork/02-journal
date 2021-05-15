ee714cf4bcc3f85b0fac17365cd15fdf0bac0b45
Agney <agney.menon@bigbinary.com>
2021-05-16 04:27:03 +0530

    add status update form to success component

--- app/javascript/components/User/Scan/ComponentByState.js
+++ app/javascript/components/User/Scan/ComponentByState.js
@@ -1,28 +1,17 @@
 import React from "react";
+import Spinner from "Common/Spinner";
+
 import QrScanner from "./QrScanner";
 import Success from "./Success";
 import Error from "./Error";
 import states from "./states.js";
-import Spinner from "Common/Spinner";
 
-const ComponentByState = ({
-  state,
-  onScanned,
-  onError,
-  merchantDetails,
-  resetState,
-}) => {
+const ComponentByState = ({ state, onScanned, onError, resetState }) => {
   switch (state) {
     case states.SCANNING:
       return <QrScanner onScanned={onScanned} onError={onError} />;
     case states.SUCCESS:
-      return (
-        <Success
-          name={merchantDetails.name}
-          address={merchantDetails.address}
-          resetState={resetState}
-        />
-      );
+      return <Success resetState={resetState} />;
     case states.ERROR_QR_READ:
       return <Error state={state} resetState={resetState} />;
     case states.ERROR_MERCHANT:
