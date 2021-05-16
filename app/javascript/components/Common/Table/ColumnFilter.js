4890f4d22cf0d1b6b789812adf66b4970c12c980
Agney <agney.menon@bigbinary.com>
2021-05-16 23:40:07 +0530

    change icon on column filter

--- app/javascript/components/Common/Table/ColumnFilter.js
+++ app/javascript/components/Common/Table/ColumnFilter.js
@@ -6,13 +6,14 @@ import { useClickAway } from "ahooks";
 function ColumnFilter({ column }) {
   const { filterValue, preFilteredRows, setFilter, filterable } = column;
   const [isOpen, setIsOpen] = useState(!!filterValue);
+  const containerRef = useRef(null);
   const inputRef = useRef(null);
 
   useClickAway(() => {
     if (!filterValue) {
       setIsOpen(false);
     }
-  }, inputRef);
+  }, containerRef);
 
   useEffect(() => {
     if (isOpen && inputRef.current) {
@@ -28,28 +29,31 @@ function ColumnFilter({ column }) {
   const handleToggle = (event) => {
     event.stopPropagation();
     setIsOpen((o) => !o);
+    setFilter(undefined);
   };
 
   return (
     <>
       <button title="Filter" onClick={handleToggle}>
-        <Icon icon={IconNames.FILTER} />
+        <Icon icon={filterValue ? IconNames.FILTER_KEEP : IconNames.FILTER} />
       </button>
-      {isOpen && (
-        <div className="absolute w-40">
-          <input
-            name="search"
-            ref={inputRef}
-            value={filterValue || ""}
-            onChange={(e) => {
-              const value = e.target.value;
-              setFilter(value || undefined);
-            }}
-            className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 mt-2"
-            placeholder={`Search ${count} records...`}
-          />
-        </div>
-      )}
+      <div className="absolute" ref={containerRef}>
+        {isOpen && (
+          <div className="w-40">
+            <input
+              ref={inputRef}
+              name="search"
+              value={filterValue || ""}
+              onChange={(e) => {
+                const value = e.target.value;
+                setFilter(value || undefined);
+              }}
+              className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 mt-2"
+              placeholder={`Search ${count} records`}
+            />
+          </div>
+        )}
+      </div>
     </>
   );
 }
