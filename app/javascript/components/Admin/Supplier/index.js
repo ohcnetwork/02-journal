1e8714ef6d0737ea4c21659f0a4098f6412a7939
Agney <agney.menon@bigbinary.com>
2021-05-16 14:56:36 +0530

    fix height for generate form

--- app/javascript/components/Admin/Supplier/index.js
+++ app/javascript/components/Admin/Supplier/index.js
@@ -30,7 +30,7 @@ function Supplier() {
               <button
                 type="button"
                 className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                onClick={() => history.push(`/admin/supplier/new`)}
+                onClick={() => history.push(`/admin/suppliers/new`)}
               >
                 Create new Supplier
               </button>
