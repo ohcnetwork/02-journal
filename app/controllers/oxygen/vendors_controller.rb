2e72fd26aa97424cd7a0267c62a0a163cf7bf98b
Unnikrishnan KP <unni.tallman@gmail.com>
2021-05-16 23:24:38 +0530

    ids parameter added for qr_codes display page

--- app/controllers/oxygen/vendors_controller.rb
+++ app/controllers/oxygen/vendors_controller.rb
@@ -3,6 +3,10 @@
 class Oxygen::VendorsController < ApplicationController
   def qr_codes
     @vendor = Oxygen::Vendor.find(params[:id])
-    @cylinders = @vendor.cylinders
+    if params[:ids].present?
+      @cylinders = @vendor.cylinders.where(id: params[:ids].split(","))
+    else
+      @cylinders = @vendor.cylinders
+    end
   end
 end
