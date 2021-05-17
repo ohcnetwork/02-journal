af01ede8e6972e2760f0b66409ff4adc6c2d7eff
Agney <agney.menon@bigbinary.com>
2021-05-17 22:22:57 +0530

    move options to common component

---
+++ app/javascript/components/Admin/Cylinder/cylinderParams.js
@@ -0,0 +1,56 @@
+export const capacityOptions = [
+  {
+    label: "D",
+    value: "d",
+  },
+  {
+    label: "B",
+    value: "B",
+  },
+  {
+    label: "C",
+    value: "c",
+  },
+  {
+    label: "H",
+    value: "h",
+  },
+];
+
+export const statusOptions = [
+  {
+    label: "Filled",
+    value: "filled",
+  },
+  {
+    label: "Partially",
+    value: "Partial",
+  },
+  {
+    label: "Empty",
+    value: "empty",
+  },
+  {
+    label: "Faulty",
+    value: "faulty",
+  },
+];
+
+export const typeOptions = [
+  {
+    label: "Ind 0₂",
+    value: "ind",
+  },
+  {
+    label: "Med 0₂",
+    value: "med",
+  },
+  {
+    label: "Nitrogen",
+    value: "nitrogen",
+  },
+  {
+    label: "Argon",
+    value: "arg",
+  },
+];
