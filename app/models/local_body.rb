c2ca5481709d14ae064aec91e0ad9653da75b2d2
Unnikrishnan KP <unni.tallman@gmail.com>
2021-05-18 23:34:01 +0530

    CRUD API for Stations added

--- app/models/local_body.rb
+++ app/models/local_body.rb
@@ -3,7 +3,7 @@
 class LocalBody < ApplicationRecord
   acts_as_copy_target
 
-  has_many :merchants, foreign_key: :lb_code, dependent: :destroy, inverse_of: :local_bodies
+  has_many :stations, foreign_key: :lb_code, dependent: :destroy, inverse_of: :local_bodies
 
   def self.formatted
     @@_lb_data ||= LocalBodyDataFormatterService.new.run!
